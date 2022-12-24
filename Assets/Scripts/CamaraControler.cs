9148768a53af198fee3139494b883ce048a2b7af
jaimebanos <jaimebanossoriano@gmail.com>
2022-12-24 19:53:09 +0100

    Optimización movimiento del mapa
    Ahora el mapa acompañará al personaje también sobre el vector y

--- Assets/Scripts/CamaraControler.cs
+++ Assets/Scripts/CamaraControler.cs
@@ -10,25 +10,30 @@ public class CamaraControler : MonoBehaviour
     public Transform baground, middleBackground, othermiddleBackground;
 
     private float _lastPos;
-    // Start is called before the first frame update
+    private float _lastPosy;
+    
     void Start()
     {
-        _lastPos = transform.position.x;
+        _lastPos = transform.position.x; //POSICION ANTERIOR DEL BACKGROUND VEC X
+        _lastPosy = transform.position.y; //POSICION ANTERIOR DEL BACKGROUND VEC Y
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (target != null)
         {
-            transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
-            float ammountToMove = transform.position.x - _lastPos;
+            //LA NUEVA POSICION DE LA CAMARA CON EL VECTOR X DEL PLAYER Y EL Y;
+            transform.position = new Vector3(target.position.x, target.position.y + 2f, transform.position.z);
+            float ammountToMoveX = transform.position.x - _lastPos; //LA POSICION QUE SE TIENE QUE MOVER VECTOR X
+            float ammontToMovey = transform.position.y - _lastPosy;  //LA POSICION QUE SE TIENE QUE MOVER VECTOR Y
 
-            baground.position += new Vector3(ammountToMove, 0f, 0f);
-            middleBackground.position += new Vector3(ammountToMove * 0.1f, 0f, 0f);
-            othermiddleBackground.position += new Vector3(ammountToMove * 0.1f, 0f, 0f);
+            baground.position += new Vector3(ammountToMoveX, ammontToMovey, 0f); //BACKGROUND
+            middleBackground.position += new Vector3(ammountToMoveX * 0.1f, 0f, 0f); //FONDOMAPA
+            othermiddleBackground.position += new Vector3(ammountToMoveX * 0.1f, 0f, 0f); //MONTAÑAS
 
-            _lastPos = transform.position.x;
+            _lastPos = transform.position.x; //POSICION ANTERIOR DEL BACKGROUND VEC X
+            _lastPosy = transform.position.y; //POSICION ANTERIOR DEL BACKGROUND VEC Y
         }
     }
 }
