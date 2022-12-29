523c5b84866a79ef58fa905b5093f888f6b39c08
KwAhmama <kawtar.ahmama@gmail.com>
2022-12-29 15:49:54 +0100

    Recoger objetos inventario
    Por solucionar pequeño fallo

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-   public static InventoryManager Instance;
-   public List<Item> Items = new List <Item>();
-   
-   
-   
-   private void Awake() {
-    Instance = this;
-   } 
+    public static InventoryManager Instance;
+    public List<Item> Items = new List<Item>();
 
-   public void Add(Item item)
-   {
-    Items.Add(item);
-   }
-   
-   public void Remove(Item item)
-   {
-    Items.Remove(item);
-   }
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+        Debug.Log("An item was added to the item list");
+    }
+
+    public void Remove(Item item)
+    {
+        Items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in Items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+        }
+    }
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "Item",menuName = "Item/Create New Item")]
-public class Item : ScriptableObject {
-    
+[CreateAssetMenu(fileName = "New Item" , menuName = "Item/Create New Item")]
+
+public class Item : ScriptableObject
+{
     public int id;
     public string itemName;
     public int value;
