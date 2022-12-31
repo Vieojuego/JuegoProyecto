28d518b2cdbad950bcc10fcf5c3f198818ad5232
KwAhmama <kawtar.ahmama@gmail.com>
2022-12-31 14:30:24 +0100

    Remove Items from Inventory
    Se pueden eliminar los objetos del inventario

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -14,6 +14,9 @@ public class InventoryManager : MonoBehaviour
 
     public Transform ItemContent;
     public GameObject InventoryItem;
+    public Toggle RemoveEnable;
+    public InventoryItemController[] InventoryItems;
+
 
     private void Awake()
     {
@@ -38,21 +41,53 @@ public class InventoryManager : MonoBehaviour
 
     public void ListItems()
     {
-        
+
+
         foreach (var item in items_add)
         {
+            
             GameObject obj = Instantiate(InventoryItem, ItemContent);
             var itemName = obj.transform.Find("ItemName").GetComponent<TMP_Text>();
             var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            var itemRemoveBt = obj.transform.Find("RemoveItem").GetComponent<Button>();
+
             itemName.text = item.itemName;
             itemIcon.sprite = item.icon;
             Items.Add(item);
+            if(RemoveEnable.isOn)
+            {
+                itemRemoveBt.gameObject.SetActive(true);
+            }
         }
 
         items_add.Clear();
+        SetInventoryItems();
         
     }
 
-    
-    
-}
+    public void EnableItemsRemove(){
+        if(RemoveEnable.isOn)
+        {
+
+            foreach(Transform item in ItemContent) {
+                item.Find("RemoveItem").gameObject.SetActive(true);
+
+            }
+        }else
+        {
+             foreach(Transform item in ItemContent) {
+                item.Find("RemoveItem").gameObject.SetActive(false);
+
+            }
+        }
+    }
+
+    public void SetInventoryItems() 
+    {
+        InventoryItems  = ItemContent.GetComponentsInChildren<InventoryItemController>();
+
+        for(int i = 0; i < Items.Count; i++) {
+            InventoryItems[i].AddItem(Items[i]);
+        }
+    }
+} 
